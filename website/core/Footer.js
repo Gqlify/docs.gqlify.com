3965abd3dc72e4fb34b963f7f6b49a80a31a6bcf
william chang <wwwy3y3@gmail.com>
2018-12-05 15:02:33 +0800

    update footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -34,30 +34,27 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('why-gqlify', this.props.language)}>
+              Why GQLify
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl('intro', this.props.language)}>
+              Quick Start
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+            <a href={this.docUrl('data-model-overview', this.props.language)}>
+              Data Model
+            </a>
+            <a href={this.docUrl('data-source-overview', this.props.language)}>
+              Data Source
+            </a>
+            <a href={this.docUrl('relationship-overview', this.props.language)}>
+              Data Relationship
             </a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
+            <a href="https://gitter.im/Canner/gqlify">Gitter Chat</a>
             <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
+              href="https://twitter.com/cannerIO"
               target="_blank"
               rel="noreferrer noopener">
               Twitter
@@ -65,13 +62,12 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+            <a href="https://github.com/canner/gqlify">GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
+              data-count-href="/canner/gqlify/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub">
@@ -81,15 +77,13 @@ class Footer extends React.Component {
         </section>
 
         <a
-          href="https://code.facebook.com/projects/"
+          href="https://www.canner.io"
           target="_blank"
           rel="noreferrer noopener"
           className="fbOpenSource">
           <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
+            src={`https://www.canner.io/img/logo-word-white.png`}
             alt="Facebook Open Source"
-            width="170"
-            height="45"
           />
         </a>
         <section className="copyright">{this.props.config.copyright}</section>
